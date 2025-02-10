a440f8fdb69ad1e4f894e543f827f886b60bc18b
Cici Saepudin <cici.syaepudin@gmail.com>
2025-02-10 10:01:48 +0700

    Description : fix filter on cancel order page

--- FRS/FRS/ClientApp/src/app/components/meal-order-system/cancel-order-request/cancel-order-requests-management.component.ts
+++ FRS/FRS/ClientApp/src/app/components/meal-order-system/cancel-order-request/cancel-order-requests-management.component.ts
@@ -155,6 +155,9 @@ export class CancelOrderRequestsManagementComponent implements OnInit {
 
   onSearchChanged(value: string) {
     this.keyword = value;
+  }
+
+  onEnterSearchPress() {
     this.clearFilterAndPagedResult();
     this.loadData(null);
   }
