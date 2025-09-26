d988d3ba9220dbf05b0dff0799f9e7c808252cbf
mochammadtri <mochammad.tri@gmail.com>
2025-09-26 08:38:03 +0700

    fix merge

--- FRS/FRS/Startup.cs
+++ FRS/FRS/Startup.cs
@@ -634,8 +634,6 @@ namespace FRS
             services.AddScoped<IPaymentService, PaymentService>();
             services.AddScoped<IManagementService, ManagementService>();
             services.AddScoped<IUserActivityRepository, UserActivityRepository>();
-            services.AddScoped<IStudentWalletService, StudentWalletService>();
-            services.AddScoped<IStudentPointService, StudentpointService>();
 
             services.Configure<FormOptions>(o =>  // currently all set to max, configure it to your needs!
             {
