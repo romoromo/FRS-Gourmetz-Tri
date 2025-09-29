f7d918eca029ff061e6925eceb1305a3a9b00df9
mochammadtri <mochammad.tri@gmail.com>
2025-09-29 14:08:12 +0700

    freeze limit db update

---
+++ FRS/FRS/Migrations/20250929050733_wallet-freeze-limit.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class walletfreezelimit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWalletFreeze",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "WalletDailyLimit",
+                table: "Students",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWalletFreeze",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "WalletDailyLimit",
+                table: "Students");
+        }
+    }
+}
