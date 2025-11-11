90e0d5c535ed15fe10dfb8877346e67ef977f0f8
M Tri R <mochammad.tri@gmail.com>
2025-11-11 19:32:32 +0700

    test missing

---
+++ FRS/FRS/Migrations/20251111002149_testMissing.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+namespace FRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class testMissing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
