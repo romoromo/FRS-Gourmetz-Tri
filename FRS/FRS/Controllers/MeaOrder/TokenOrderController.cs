e5af69de1298f445b676128439475387a7933193
M Tri R <mochammad.tri@gmail.com>
2025-12-10 18:47:21 +0700

    comment out for faster load on portal,
    as not used on stripe and wallet payment for gourmetz

--- FRS/FRS/Controllers/MeaOrder/TokenOrderController.cs
+++ FRS/FRS/Controllers/MeaOrder/TokenOrderController.cs
@@ -95,7 +95,8 @@ namespace FRS.Controllers
         [ProducesResponseType(403)]
         public async Task<IActionResult> GetTokenOrdersOrderingPortal(RequestFilter filter)
         {
-            if (filter.isrequest) await _orderController.QueryAndUpdatePaymentStatus(filter.studentId);
+            //commented out because payment only using wallet, stripe also dont use this,
+            //if (filter.isrequest) await _orderController.QueryAndUpdatePaymentStatus(filter.studentId);
 
             PagedEntity<TokenOrderOrderingPortalDTO> results = await this._service.GetTokenOrdersOrderingPortalAsync(filter);
             return Ok(results);
