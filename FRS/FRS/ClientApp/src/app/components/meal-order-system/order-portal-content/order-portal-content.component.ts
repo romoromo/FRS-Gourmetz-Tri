3f582b5b3a782fae2c78695bdb98057b055074f4
Harizal Hilmi <harizalhilmi@gmail.com>
2025-12-26 16:24:47 +0700

    #130
    fix download issue

--- FRS/FRS/ClientApp/src/app/components/meal-order-system/order-portal-content/order-portal-content.component.ts
+++ FRS/FRS/ClientApp/src/app/components/meal-order-system/order-portal-content/order-portal-content.component.ts
@@ -1,44 +1,51 @@
-import { Component, ViewChild, Inject } from '@angular/core';
+import { Component, ViewChild, Inject } from "@angular/core";
 
-import { AlertService, MessageSeverity } from '../../../services/alert.service';
+import { AlertService, MessageSeverity } from "../../../services/alert.service";
 import { AccountService } from "../../../services/account.service";
-import { Permission } from '../../../models/permission.model';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { MealService } from 'src/app/services/meal-order/meal.service';
-import { OrderPortalService } from 'src/app/services/order-portal.service';
-import { OrderPortalBanner, OrderPortalContent } from 'src/app/models/meal-order/order-portal-content.model';
-import { FileService } from 'src/app/services/file.service';
-import { getBaseUrl } from 'src/app/app.module';
-
+import { Permission } from "../../../models/permission.model";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { MealService } from "src/app/services/meal-order/meal.service";
+import { OrderPortalService } from "src/app/services/order-portal.service";
+import {
+  OrderPortalBanner,
+  OrderPortalContent,
+} from "src/app/models/meal-order/order-portal-content.model";
+import { FileService } from "src/app/services/file.service";
+import { getBaseUrl } from "src/app/app.module";
 
 @Component({
-  selector: 'order-portal-content-editor',
-  templateUrl: './order-portal-content.component.html',
-  styleUrls: ['./order-portal-content.component.css']
+  selector: "order-portal-content-editor",
+  templateUrl: "./order-portal-content.component.html",
+  styleUrls: ["./order-portal-content.component.css"],
 })
 export class OrderPortalContentEditorComponent {
-
   private isNewOrderPortalContent = false;
   private isSaving: boolean;
   private showValidationErrors: boolean = true;
   private editingOrderPortalContentCode: string;
   private portalContentEdit: OrderPortalContent = new OrderPortalContent();
   private allPermissions: Permission[] = [];
-  private selectedValues: { [key: string]: boolean; } = {};
+  private selectedValues: { [key: string]: boolean } = {};
   public formResetToggle = true;
   public outletId: string;
   public changesSavedCallback: () => void;
   public changesFailedCallback: () => void;
   public changesCancelledCallback: () => void;
-  public fileUploadResponse: { dbPath: '', fileId: null, fileName: '' };
+  public fileUploadResponse: { dbPath: ""; fileId: null; fileName: "" };
 
-  @ViewChild('f')
+  @ViewChild("f")
   private form;
 
-  constructor(private alertService: AlertService, private portalService: OrderPortalService, private accountService: AccountService,
-    public dialogRef: MatDialogRef<OrderPortalContentEditorComponent>, private mealService: MealService, private fileService: FileService,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
-    if (typeof (data.portalContent) != typeof (undefined)) {
+  constructor(
+    private alertService: AlertService,
+    private portalService: OrderPortalService,
+    private accountService: AccountService,
+    public dialogRef: MatDialogRef<OrderPortalContentEditorComponent>,
+    private mealService: MealService,
+    private fileService: FileService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    if (typeof data.portalContent != typeof undefined) {
       this.outletId = data.outletId;
       if (data.portalContent.id) {
         this.editOrderPortalContent(data.portalContent);
@@ -48,64 +55,74 @@ export class OrderPortalContentEditorComponent {
     }
   }
 
-
   private showErrorAlert(caption: string, message: string) {
     this.alertService.showMessage(caption, message, MessageSeverity.error);
   }
 
-
   private save() {
     this.isSaving = true;
     this.alertService.startLoadingMessage("Saving changes...");
-    
 
     if (this.isNewOrderPortalContent) {
-      this.portalService.newOrderPortalContent(this.portalContentEdit).subscribe(portalContent => this.saveSuccessHelper(portalContent), error => this.saveFailedHelper(error));
-    }
-    else {
-      this.portalService.updateOrderPortalContent(this.portalContentEdit).subscribe(response => this.saveSuccessHelper(), error => this.saveFailedHelper(error));
+      this.portalService
+        .newOrderPortalContent(this.portalContentEdit)
+        .subscribe(
+          (portalContent) => this.saveSuccessHelper(portalContent),
+          (error) => this.saveFailedHelper(error)
+        );
+    } else {
+      this.portalService
+        .updateOrderPortalContent(this.portalContentEdit)
+        .subscribe(
+          (response) => this.saveSuccessHelper(),
+          (error) => this.saveFailedHelper(error)
+        );
     }
   }
 
-
   private saveSuccessHelper(portalContent?: OrderPortalContent) {
-    if (portalContent)
-      Object.assign(this.portalContentEdit, portalContent);
+    if (portalContent) Object.assign(this.portalContentEdit, portalContent);
 
     this.isSaving = false;
     this.alertService.stopLoadingMessage();
     this.showValidationErrors = false;
 
     if (this.isNewOrderPortalContent)
-      this.alertService.showMessage("Success", `Content was created successfully`, MessageSeverity.success);
+      this.alertService.showMessage(
+        "Success",
+        `Content was created successfully`,
+        MessageSeverity.success
+      );
     else
-      this.alertService.showMessage("Success", `Changes to the content was saved successfully`, MessageSeverity.success);
-
+      this.alertService.showMessage(
+        "Success",
+        `Changes to the content was saved successfully`,
+        MessageSeverity.success
+      );
 
     this.portalContentEdit = new OrderPortalContent();
     this.resetForm();
 
-
     //if (!this.isNewOrderPortalContent && this.accountService.currentUser.facilities.some(r => r == this.editingOrderPortalContentCode))
     //    this.refreshLoggedInUser();
 
-    if (this.changesSavedCallback)
-      this.changesSavedCallback();
+    if (this.changesSavedCallback) this.changesSavedCallback();
 
     this.dialogRef.close();
   }
 
-
   private saveFailedHelper(error: any) {
     this.isSaving = false;
     this.alertService.stopLoadingMessage();
-    this.alertService.showStickyMessage("Save Error", "The below errors occured while saving your changes:", MessageSeverity.error);
+    this.alertService.showStickyMessage(
+      "Save Error",
+      "The below errors occured while saving your changes:",
+      MessageSeverity.error
+    );
     this.alertService.showStickyMessage(error, null, MessageSeverity.error);
 
-    if (this.changesFailedCallback)
-      this.changesFailedCallback();
+    if (this.changesFailedCallback) this.changesFailedCallback();
   }
-
 
   private cancel() {
     this.portalContentEdit = new OrderPortalContent();
@@ -115,18 +132,15 @@ export class OrderPortalContentEditorComponent {
 
     this.alertService.resetStickyMessage();
 
-    if (this.changesCancelledCallback)
-      this.changesCancelledCallback();
+    if (this.changesCancelledCallback) this.changesCancelledCallback();
 
     this.dialogRef.close();
   }
 
   resetForm(replace = false) {
-
     if (!replace) {
       this.form.reset();
-    }
-    else {
+    } else {
       this.formResetToggle = false;
 
       setTimeout(() => {
@@ -134,7 +148,6 @@ export class OrderPortalContentEditorComponent {
       });
     }
   }
-
 
   newOrderPortalContent() {
     this.isNewOrderPortalContent = true;
@@ -158,8 +171,7 @@ export class OrderPortalContentEditorComponent {
       Object.assign(this.portalContentEdit, portalContent);
 
       return this.portalContentEdit;
-    }
-    else {
+    } else {
       return this.newOrderPortalContent();
     }
   }
@@ -173,14 +185,15 @@ export class OrderPortalContentEditorComponent {
   removeBanner(compo) {
     if (!this.portalContentEdit.banners) this.portalContentEdit.banners = [];
     const indx = this.portalContentEdit.banners.indexOf(compo);
-    if (indx > -1)
-      this.portalContentEdit.banners.splice(indx, 1);
+    if (indx > -1) this.portalContentEdit.banners.splice(indx, 1);
   }
 
   public uploadBannerImageFinished = (event, banner) => {
     this.fileUploadResponse = event;
-    banner.imageFilePath = this.fileUploadResponse ? this.fileUploadResponse.dbPath : null;
-  }
+    banner.imageFilePath = this.fileUploadResponse
+      ? this.fileUploadResponse.dbPath
+      : null;
+  };
 
   removePhoto(banner) {
     banner.imageFilePath = null;
@@ -190,18 +203,26 @@ export class OrderPortalContentEditorComponent {
   public uploadBannerFinished = (event, banner) => {
     this.fileUploadResponse = event;
     banner.fileName = event.fileName;
-    banner.filePath = this.fileUploadResponse ? this.fileUploadResponse.dbPath : null;
-  }
+    banner.filePath = this.fileUploadResponse
+      ? this.fileUploadResponse.dbPath
+      : null;
+  };
 
   getFileImage(path) {
     return this.fileService.getFile(path);
   }
 
   downloadFile(path) {
-    window.open(getBaseUrl() + '/gateway/Download/FileByPath?filePath=/' + encodeURIComponent(path), '_blank');
-  }
+    //window.open(getBaseUrl() + '/gateway/Download/FileByPath?filePath=/' + encodeURIComponent(path), '_blank');
+
+    const normalizedPath = path.replace(/\\/g, "/");
+    const fullPath  = (getBaseUrl() + encodeURIComponent(normalizedPath).replace(/%2F/g, "/")).replace("webadmin/", "");
+    window.open(fullPath, "_blank"); 
+   }
 
   get canManageOrderPortalContents() {
-    return this.accountService.userHasPermission(Permission.manageMOSOutletMgtPortalContentsPermission)
+    return this.accountService.userHasPermission(
+      Permission.manageMOSOutletMgtPortalContentsPermission
+    );
   }
 }
