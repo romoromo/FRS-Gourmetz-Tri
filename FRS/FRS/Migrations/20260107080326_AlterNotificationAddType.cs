f6e5ab555a96133396706cdfd34ed33de36fc447
Harizal Hilmi <harizalhilmi@gmail.com>
2026-01-07 15:56:17 +0700

    add migrations

---
+++ FRS/FRS/Migrations/20260107080326_AlterNotificationAddType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterNotificationAddType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Notifications",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Notifications");
+        }
+    }
+}
