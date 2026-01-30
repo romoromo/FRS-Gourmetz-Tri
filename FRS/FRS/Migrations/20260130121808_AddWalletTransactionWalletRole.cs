300c904e90a7da363a6d91bc29f6c2044df93543
Harizal Hilmi <harizalhilmi@gmail.com>
2026-01-30 19:26:59 +0700

    add migration

---
+++ FRS/FRS/Migrations/20260130121808_AddWalletTransactionWalletRole.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWalletTransactionWalletRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DECLARE @Role VARCHAR(50)
+                SELECT @Role = Id FROM [Role] WHERE Name = 'CAT_ADMIN'
+
+                IF NOT EXISTS (SELECT * FROM UserRoleClaim WHERE ClaimValue = 'mosmgt.reportmgt.wallettransaction.view')
+                BEGIN
+	                INSERT INTO UserRoleClaim (RoleId, ClaimType, ClaimValue) VALUES
+	                (@Role, 'permission', 'mosmgt.reportmgt.wallettransaction.view')
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
