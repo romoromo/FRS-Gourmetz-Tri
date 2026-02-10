4e1b58a217b322822e6fa7086bab953bf4470368
M Tri R <mochammad.tri@gmail.com>
2026-02-10 16:15:55 +0700

    db update for posinvoiceid

---
+++ FRS/FRS/Migrations/20260210090932_posinvoiceid.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class posinvoiceid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PosInvoiceId",
+                table: "Payments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PosInvoiceId",
+                table: "Payments");
+        }
+    }
+}
